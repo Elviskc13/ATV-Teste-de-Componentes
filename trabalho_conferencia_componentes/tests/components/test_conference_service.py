7118e67f6a1e3273f5dde80fa12ee0992d6d95dc
Elviskc <elvis8.ek@gmail.com>
2026-05-06 07:59:31 -0300

    testes de entrada na fila de espera

--- trabalho_conferencia_componentes/tests/components/test_conference_service.py
+++ trabalho_conferencia_componentes/tests/components/test_conference_service.py
@@ -52,3 +52,38 @@ def test_participante_nao_pagou():
     result = service.enroll_in_workshop(30, 1)
 
     assert result is False
+
+def test_entrada_na_fila():
+    workshop_repo = WorkshopRepository()
+    participant_repo = ParticipantRepository()
+    enrollment_repo = EnrollmentRepository()
+    waitlist_repo = WaitlistRepository()
+
+    service = ConferenceService(
+        workshop_repo,
+        participant_repo,
+        enrollment_repo,
+        waitlist_repo
+    )
+
+    result = service.join_waitlist(10, 3)
+
+    assert result is True
+
+
+def test_nao_entra_na_fila_se_disponivel():
+    workshop_repo = WorkshopRepository()
+    participant_repo = ParticipantRepository()
+    enrollment_repo = EnrollmentRepository()
+    waitlist_repo = WaitlistRepository()
+
+    service = ConferenceService(
+        workshop_repo,
+        participant_repo,
+        enrollment_repo,
+        waitlist_repo
+    )
+
+    result = service.join_waitlist(10, 1)
+
+    assert result is False
